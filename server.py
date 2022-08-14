8514ed9f87f900aa6c020a4463f8f286402cf1e5
bishwas-py <bishwasbh@gmail.com>
2022-08-14 22:18:54 +0545

    Context reader added

--- server.py
+++ server.py
@@ -1,28 +1,40 @@
 import re
-from pathlib import Path
 import os
+import importlib
+from pathlib import Path
+
 BASE_DIR = Path(__file__).resolve().parent
 template_path = "templates"
-def render_template(template_name="index.html"):
-    return "<h1>Hello</h1>"
-
 
 templates_dir = os.listdir(BASE_DIR / template_path)
 
+
+def render_template(path, context={}):
+    html_str = ""
+    #  paths[path] gives filename i.e. index.svelte
+    with open(BASE_DIR / f"templates/{paths[path]['fn']}", 'r') as f:
+        html_str = f.read()
+        mod = importlib.import_module(f"controllers.{paths[path]['ap']}")
+        html_str = html_str.format(**mod.context)
+        return html_str
+
+
 paths = {}
 for file_name in templates_dir:
-    path_name = re.sub(r'\.pml$', '', file_name)
-    if path_name == "index":
-        path_name = "/"
+    path = re.sub(r'\.svelte$', '', file_name)
+    actual_path = path
+    if path == "index":
+        path = "/"
     paths.update(
         {
-            path_name: file_name
+            path: {'fn': file_name, 'ap': actual_path}
         }
     )
 
+
 def app(environ, start_response):
-    print(paths)
-    data = render_template()
+    path = environ.get("PATH_INFO")
+    data = render_template(path)
     data = data.encode("utf-8")
 
     start_response(
