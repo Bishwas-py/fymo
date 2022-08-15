1f8e6a02debcec8bc05044f1e344ceac5e98fb4e
bishwas-py <bishwasbh@gmail.com>
2022-08-15 08:08:18 +0545

    Simple error handling

--- generate.py
+++ generate.py
@@ -6,19 +6,9 @@ import re
 
 import pathlib
 
+from rigids import SColor
+
 currentPath = pathlib.Path(__file__).parent.resolve()
-
-
-class SColor:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKCYAN = '\033[96m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
 
 
 def generate_content(params, handler_name, content, extension):
--- server.py
+++ server.py
@@ -1,46 +1,10 @@
-import re
-import os
 import importlib
 from pathlib import Path
 
-from routes.config import get_routes
+from rigids import SColor
+from routes.config import paths
 
 BASE_DIR = Path(__file__).resolve().parent
-templates_root = "templates"
-
-routes = get_routes()
-
-paths = {}
-
-root_path = routes.get('root')
-# sets the global root path domain.com/
-if root_path:
-    file_name = f"{root_path.replace('.', '/')}.svelte"
-    paths.update({
-        '/': {'template_path': file_name, 'controller_path': root_path}
-    })
-
-# sets template and controller path through given routes
-resources = routes.get('resources')
-print(f"resources {resources}")
-if resources:
-    for resource in resources:
-
-        print(f"{templates_root}/{resource}")
-        templates_dir = os.listdir(BASE_DIR / f"{templates_root}/{resource}")
-
-        for template_file_name in templates_dir:
-            no_extension_file_path = re.sub(r'\.svelte$', '', template_file_name)
-            controller_path = f"{resource}.{no_extension_file_path}"
-            template_path = f"{resource}/{template_file_name}"
-            paths.update(
-                {
-                    f"{resource}/{no_extension_file_path}": {
-                        'template_path': template_path,
-                        'controller_path': controller_path
-                    }
-                }
-            )
 
 
 def render_template(path):
@@ -51,25 +15,29 @@ def render_template(path):
         path = path.lstrip('/')
 
     #  paths[path] gives filename i.e. index.svelte
-    print(paths[path])
-    with open(BASE_DIR / f"templates/{paths[path]['template_path']}", 'r') as f:
-        html_str = f.read()
-        print(paths[path])
-        mod = importlib.import_module(f"controllers.{paths[path]['controller_path']}")
-        html_str = html_str.format(**mod.context)
-    return html_str
+    try:
+        with open(BASE_DIR / f"templates/{paths[path]['template_path']}", 'r') as f:
+            html_str = f.read()
+            print(paths[path])
+            mod = importlib.import_module(f"controllers.{paths[path]['controller_path']}")
+            html_str = html_str.format(**mod.context)
+        return html_str, "200 OK"
+    except KeyError as e:
+        error_message = f"400: {str(e)} not found"
+        print(f"{SColor.FAIL}{error_message}{SColor.ENDC}")
+        return f"{error_message}", "400 NOT FOUND"
 
 
 # main server handler
 def app(environ, start_response):
     path = environ.get("PATH_INFO")
-    data = render_template(path)
-    data = data.encode("utf-8")
+    html_raw, response = render_template(path)
+    html = html_raw.encode("utf-8")
     start_response(
-        f"200 OK", [
+        response, [
             ("Content-Type", "text/html"),
-            ("Content-Length", str(len(data)))
+            ("Content-Length", str(len(html)))
         ]
     )
 
-    return iter([data])
+    return iter([html])
